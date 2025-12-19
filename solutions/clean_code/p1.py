b4004968302872615a9a1a643cd2b05aab19a411
Nasir <nasirhusain1137@gmail.com>
2025-12-19 18:11:44 +0530

    p1

---
+++ solutions/clean_code/p1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace(".", "[.]")
+
+sol = Solution()
+print(sol.defangIPaddr("1.1.1.1"))
+print(sol.defangIPaddr("255.100.50.0"))
