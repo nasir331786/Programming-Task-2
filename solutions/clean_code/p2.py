79b936fca42b3bf05761d790a605f83372b63725
Nasir <nasirhusain1137@gmail.com>
2025-12-19 18:12:17 +0530

    p2
    Implement a method to count numbers with even digits.

---
+++ solutions/clean_code/p2.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+        for num in nums:
+            if len(str(abs(num))) % 2 == 0:
+                count += 1
+        return count
+
+sol = Solution()
+print(sol.findNumbers([12, 345, 2, 6, 7896]))
+print(sol.findNumbers([555, 901, 482, 1771]))
